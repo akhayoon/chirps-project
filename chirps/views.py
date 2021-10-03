72fa1ef8920f07efdfa711eb540d39ab89116ac1
Hussien Khayoon <hussien@vendorhero.com>
2021-10-03 12:13:52 -0400

    update

--- chirps/views.py
+++ chirps/views.py
@@ -1,5 +1,7 @@
 from chirps.models import Chirp
 from django.views import View
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 
 from django.http import JsonResponse
 
